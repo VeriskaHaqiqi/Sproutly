3a62f55d75be9d3a143696875a64587c03305ff3
adeluln <adelulinn@gmail.com>
2026-03-17 22:22:19 +0700

    edit router

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
 Route::get('/', function () {
     return view('index');
 });
@@ -26,7 +25,6 @@ Route::get('/dashboard-user', function () {
 Route::get('/dashboard-ahli', function () {
     return view('dashboard-ahli');
 }); 
-*/
 
 Route::get('/homeUser', function () {
     return view('homeUser');
