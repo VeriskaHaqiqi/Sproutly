3b3e6a9cbb36f33e6279b68cf0ee0c41448286a7
isyana <isyanafajrzqi@gmail.com>
2026-03-17 22:02:55 +0700

    coba tambah homeuser yn

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+/*
 Route::get('/', function () {
     return view('index');
 });
@@ -20,4 +21,9 @@ Route::get('/dashboard-user', function () {
 
 Route::get('/dashboard-ahli', function () {
     return view('dashboard-ahli');
+}); 
+*/
+
+Route::get('/homeUser', function () {
+    return view('homeUser');
 });
