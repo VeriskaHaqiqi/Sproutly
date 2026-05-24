e0a430356dc0bc3cb73a347e025bc6a05f907f7f
isyana <isyanafajrzqi@gmail.com>
2026-05-24 22:16:46 +0700

    backend tarifahli

--- app/Models/TarifAhli.php
+++ app/Models/TarifAhli.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class TarifAhli extends Model
 {
-    //
+    protected $table = 'tarif_ahli';
+
+    protected $fillable = [
+        'ahli_botani_id',
+        'tarif',
+        'tgl_mulai_berlaku',
+        'tgl_akhir_berlaku',
+        'status_aktif',
+    ];
+
+    public function ahliBotani()
+    {
+        return $this->belongsTo(AhliBotani::class, 'ahli_botani_id');
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\PasswordResetController;
 use App\Http\Controllers\JadwalAhliController;
 use App\Http\Controllers\KonsultasiController;
 use App\Http\Controllers\ArtikelController;
+use App\Http\Controllers\TarifAhliController;
 
 Route::post('/register', [AuthController::class, 'apiRegister']);
 Route::post('/login', [AuthController::class, 'apiLogin']);
@@ -19,4 +20,8 @@ Route::get('/artikel', [ArtikelController::class, 'index']);
 Route::get('/artikel/{id}', [ArtikelController::class, 'show']);
 Route::post('/artikel', [ArtikelController::class, 'store']);
 Route::put('/artikel/{id}', [ArtikelController::class, 'update']);
-Route::delete('/artikel/{id}', [ArtikelController::class, 'destroy']);
+Route::get('/tarif-ahli', [TarifAhliController::class, 'index']);
+Route::get('/tarif-ahli/{id}', [TarifAhliController::class, 'show']);
+Route::post('/tarif-ahli', [TarifAhliController::class, 'store']);
+Route::put('/tarif-ahli/{id}', [TarifAhliController::class, 'update']);
+Route::delete('/tarif-ahli/{id}', [TarifAhliController::class, 'destroy']);Route::delete('/artikel/{id}', [ArtikelController::class, 'destroy']);
