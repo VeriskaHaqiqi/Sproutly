084fcde227d3060c4f193b0fe65d6e0ff4cda658
isyana <isyanafajrzqi@gmail.com>
2026-07-04 11:44:08 +0700

    Fix: bypass intl extension requirement

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,21 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Number;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        // MATIKAN PENGGUNAAN INTL
+        if (!extension_loaded('intl')) {
+            Number::useLocale('en');
+        }
     }
 }
